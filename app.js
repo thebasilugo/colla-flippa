bdccb64102cb08ab08a4af771bde45a753ee7aad
Basil, Ugo <115357283+thebasilugo@users.noreply.github.com>
2023-11-24 09:30:53 +0100

    update: use both simple and hex

--- app.js
+++ app.js
@@ -1,34 +1,92 @@
-const simpleBtn = document.querySelector('.simple');
-const hexBtn = document.querySelector('.hex');
-const hint = document.querySelector('.info');
+// fetch from html document
+const simpleBtn = document.querySelector(".simple");
+const hexBtn = document.querySelector(".hex");
+const simpleSection = document.querySelector("#simple");
+const hexSection = document.querySelector("#hex");
+const hint = document.querySelector(".info");
+const colorBtn = document.querySelector("#color-btn");
+const color = document.querySelector(".color");
 
-simpleBtn.style.color = 'red';
+// hard coded colors for simple section
+const colors = [
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "purple",
+  "pink",
+  "white",
+  "indigo",
+  "brown",
+  "coral",
+  "chocolate",
+  "beige",
+  "cyan",
+  "gray",
+  "grey",
+  "maroon",
+  "navy",
+  "snow",
+  "violet",
+];
 
-// simple functionality
-
-const colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "white", "indigo", "brown", "coral", "chocolate", "beige", "cyan", "gray", "grey", "maroon", "navy", "snow", "violet"];
-const colorBtn = document.getElementById("color-btn");
-const color = document.querySelector('.color');
-
-colorBtn.addEventListener('click', () => {
-  let randomNumber = getRandomNumColor();
-  document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent = colors[randomNumber];
-  colorBtn.innerHTML = 'change color';
-  runSimple();
-})
-
-simpleBtn.addEventListener('click', () => {
-  runSimple();
-})
-
-const runSimple = () => {
-  hint.style.display = 'none';
-  simpleBtn.style.color = 'red';
-  hexBtn.style.color = 'black';
-  document.body.style.backgroundColor = color.textContent;
-}
+// color options for hex section
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const getRandomNumColor = () => {
   return Math.floor(Math.random() * colors.length);
-}
+};
+
+const hexVisuals = () => {
+  simpleBtn.style.color = "black";
+  hexBtn.style.color = "red";
+};
+
+const simpleVisuals = () => {
+  simpleBtn.style.color = "red";
+  hexBtn.style.color = "black";
+};
+
+const getRandomNumHex = () => {
+  return Math.floor(Math.random() * hex.length);
+};
+
+const afterButtonClick = () => {
+  colorBtn.innerHTML = "change color";
+  hint.innerHTML = "";
+};
+
+const findSectionToRun = () => {
+  if (simpleBtn.style.color === "red") {
+    simpleFunction();
+  } else if (hexBtn.style.color === "red") {
+    hexFunction();
+  }
+};
+
+const simpleFunction = () => {
+  let randomNumber = getRandomNumColor();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+  afterButtonClick();
+  runSimple();
+};
+
+// run on window load
+simpleVisuals();
+
+const hexFunction = () => {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumHex()];
+  }
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+  afterButtonClick();
+};
+
+simpleBtn.addEventListener("click", simpleVisuals);
+hexBtn.addEventListener("click", hexVisuals);
+
+colorBtn.addEventListener("click", findSectionToRun);
