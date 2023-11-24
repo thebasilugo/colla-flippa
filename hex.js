edea55fb8969c2a03b933851b72a28ca92eee507
Basil, Ugo <115357283+thebasilugo@users.noreply.github.com>
2023-11-24 09:20:05 +0100

    update hex js file to use ES6 function naming convention

--- hex.js
+++ hex.js
@@ -7,7 +7,7 @@ const hint = document.querySelector('.info');
 hexBtn.style.color = 'red';
 
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-  colorBtn.addEventListener('click', function(){
+  colorBtn.addEventListener('click', () => {
     let hexColor = '#';
     for(let i = 0; i < 6; i++){
       hexColor += hex[getRandomNumHex()];
@@ -18,6 +18,6 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
     hint.innerHTML = '';
   })
 
-function getRandomNumHex(){
+const getRandomNumHex = () => {
   return Math.floor(Math.random() * hex.length);
 }
