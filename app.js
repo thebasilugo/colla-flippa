62c7d720e045ced354472041a723932d9afa06c2
Basil, Ugo <115357283+thebasilugo@users.noreply.github.com>
2023-11-24 09:16:23 +0100

    use ES6 arrow function convention

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const hint = document.querySelector('.info');
 
 simpleBtn.style.color = 'red';
 
-// SIMPLE functionality
+// simple functionality
 
 const colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "white", "indigo", "brown", "coral", "chocolate", "beige", "cyan", "gray", "grey", "maroon", "navy", "snow", "violet"];
 const colorBtn = document.getElementById("color-btn");
 const color = document.querySelector('.color');
 
-colorBtn.addEventListener('click', function(){
+colorBtn.addEventListener('click', () => {
   let randomNumber = getRandomNumColor();
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
@@ -18,17 +18,17 @@ colorBtn.addEventListener('click', function(){
   runSimple();
 })
 
-simpleBtn.addEventListener('click', function(){
+simpleBtn.addEventListener('click', () => {
   runSimple();
 })
 
-function runSimple(){
+const runSimple = () => {
   hint.style.display = 'none';
   simpleBtn.style.color = 'red';
   hexBtn.style.color = 'black';
   document.body.style.backgroundColor = color.textContent;
 }
 
-function getRandomNumColor(){
+const getRandomNumColor = () => {
   return Math.floor(Math.random() * colors.length);
 }
