b5937f2a92fe0ec1ee7795cd655c8a122fed5aba
Basil Ugo <basilugo2@gmail.com>
2024-11-02 14:42:14 +0100

    update

--- dist/app.js
+++ dist/app.js
@@ -110,24 +110,7 @@ const colors = [
     "saddlebrown",
     "sienna",
 ];
-const hex = [
-    0,
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const getRandomNumColor = () => {
     return Math.floor(Math.random() * colors.length);
 };
